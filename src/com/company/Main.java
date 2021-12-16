405dc8f795174efafb1dc06179a4424cbbd5c97c
IhorVlas <8952660@gmail.com>
2021-12-16 19:57:47 +0200

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double x = 2, y = 3 , a = 4, w = 2, b = 5, X = 6, e = 4;
+
+        double task1 = x+y-a/2*(x+w);
+        double task2 = a*(x+x)/y-a*3;
+        double task3 = a+b/x-y;
+        double task4 = X-e/2%e+1;
+
+        System.out.println(task1);
+        System.out.println(task2);
+        System.out.println(task3);
+        System.out.println(task4);
+    }
+}
