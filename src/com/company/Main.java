54efff4139ef2acd8a1b9c8edd73ff98ff312f70
IhorVlas <8952660@gmail.com>
2021-12-17 18:31:41 +0200

    First formatted

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        double x = 2, y = 3 , a = 4, w = 2, b = 5, X = 6, e = 4;
+        double x = 2, y = 3, a = 4, w = 2, b = 5, X = 6, e = 4;
 
-        double task1 = x+y-a/2*(x+w);
-        double task2 = a*(x+x)/y-a*3;
-        double task3 = a+b/x-y;
-        double task4 = X-e/2%e+1;
+        double task1 = x + y - a / 2 * (x + w);
+        double task2 = a * (x + x) / y - a * 3;
+        double task3 = a + b / x - y;
+        double task4 = X - e / 2 % e + 1;
 
         System.out.println(task1);
         System.out.println(task2);
